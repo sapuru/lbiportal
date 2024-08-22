5a474509a8771c0e7558f534f6aeb5eec2cdd254
aymara <aymarasamudio@gmail.com>
2024-08-22 01:23:14 -0300

    agrego radial

--- frontend/src/app/[lang]/components/charts/RadialChart.tsx
+++ frontend/src/app/[lang]/components/charts/RadialChart.tsx
@@ -58,7 +58,6 @@ export function RadialChart(){
     return   <ResponsiveContainer width="100%" height="100%">
     <RadialBarChart cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" barSize={10} data={data}>
       <RadialBar
-        minAngle={15}
         label={{ position: 'insideStart', fill: '#fff' }}
         background
         clockWise
